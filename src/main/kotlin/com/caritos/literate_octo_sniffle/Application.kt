3f97e74157b37f94379b101900e50ce4117ac97b
Eladio Caritos <eladio@nightwing.home>
2021-05-04 14:21:06 -0400

    first commit

---
+++ src/main/kotlin/com/caritos/literate_octo_sniffle/Application.kt
@@ -0,0 +1,11 @@
+package com.caritos.literate_octo_sniffle
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class LiterateOctoSniffleApplication
+
+fun main(args: Array<String>) {
+    runApplication<LiterateOctoSniffleApplication>(*args)
+}
---
+++ src/test/kotlin/com/caritos/literate_octo_sniffle/LiterateOctoSniffleApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.caritos.literate_octo_sniffle
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class LiterateOctoSniffleApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
